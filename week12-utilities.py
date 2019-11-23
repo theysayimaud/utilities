7adfdd070386be6cb6e59775e1b3aff2c5085a2b
audrey bradford <audreybradford@mymail.mines.edu>
2019-11-22 21:47:29 -0700

    updated file added function 1

---
+++ week12-utilities.py
@@ -0,0 +1,10 @@
+# Audrey Bradford
+# ​CSCI 102 – Section F
+# Week 12 - Part A
+
+################################################
+########   Function 1 : PrintOutput    #########
+################################################
+
+def PrintOutput(x):
+    print("OUTPUT", x)
