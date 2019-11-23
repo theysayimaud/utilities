df6c933f285786f1450b55a5ce05a0a2267fade1
audrey bradford <audreybradford@mymail.mines.edu>
2019-11-22 22:11:28 -0700

    added function 3

--- week12-utilities.py
+++ week12-utilities.py
@@ -17,3 +17,16 @@ def LoadFile(x):
     lines = open(x, "r")
     listedboi = lines.splitlines()
     return (listedboi)
+
+################################################
+########   Function 3 : UpdateString   #########
+################################################
+
+def UpdateString(x,y,z):
+    updated = ''
+    original = list(x)
+    original.pop(z)
+    original.insert(3, y)
+    for i in original:
+        updated += i
+    return (updated)
